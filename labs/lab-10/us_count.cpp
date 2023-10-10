6408525d8ffe39c626612784d1d4e68a1f6812e9
Luis Martinez <mrluismartinezzz@gmail.com>
2023-10-09 21:56:35 -0700

    [lab-10] adds us_count.cpp & header

---
+++ labs/lab-10/us_count.cpp
@@ -0,0 +1,4 @@
+double us_count(int q, int d, int n, int p)
+{
+    return ((q * 0.25) + (d * 0.10) + (n * 0.05) + (p * 0.01));
+}
---
+++ labs/lab-10/us_count.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int us_count(int q = 0.00, int d = 0.00, int n = 0.00, int p = 0.00);
