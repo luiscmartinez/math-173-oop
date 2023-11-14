31efe3cb52f8e7b6a8255b14dc3c68c1f56b2cb6
Luis Martinez <mrluismartinezzz@gmail.com>
2023-11-13 18:04:42 -0800

    [lab-13] logic to multiply 2x2 matrices
    matrices are represented by a one-dimensional array

--- labs/lab-13/matrix.cpp
+++ labs/lab-13/matrix.cpp
@@ -4,17 +4,28 @@ int main()
 {
     double left[] = {1, 2, 3, 4};
     double right[] = {1, 2, 1, -1};
-    double result[4];
+    double result[4] = {0.0, 0.0, 0.0, 0.0};
     // do computation, print result
-    // define totalRows
-    int totalRows = 2;
+    // define r for total number of matrix rows
+    int r = 2;
     for (int i = 1; i < 3; ++i)
     {
         for (int j = 1; j < 3; ++j)
         {
-            int space = ((i - 1) * totalRows) + (j - 1);
-            std::cout << "space is " << space << " when i is " << i << " and j is " << j << std::endl;
+            for (int k = 0; k < 2; ++k)
+            {
+                std::cout << "what is left value " << left[(i - 1) * r + k] << "\n";
+                std::cout << "what is right value " << right[j - 1 + k * r] << "\n";
+                int index = ((i - 1) * 2) + (j - 1);
+                std::cout << "what is index " << index << "\n";
+                result[index] += left[(i - 1) * r + k] * right[j - 1 + k * r];
+                std::cout << "print new result at index " << index << " which has the value is " << result[index] << "\n";
+            }
         }
     }
-    std::cout << result[0];
+    // logs out answers
+    for (int i = 0; i < 4; ++i)
+    {
+        std::cout << result[i] << "\n";
+    }
 }
