baf10a59dcdc11769558e717e6bf764468c80f3b
Luis Martinez <mrluismartinezzz@gmail.com>
2023-11-20 17:35:43 -0800

    adds class matrix example

--- exercises/class-matrix/class-matrix.h
+++ exercises/class-matrix/class-matrix.h
@@ -1,13 +1,17 @@
 #pragma once
 
 #include <ostream>
+#include <initializer_list>
 
 // Declaration
 class square_matrix
 {
 public:
-    square_matrix(int n);
-    // todo: add another constructor that accepts `{1, 3, 5, 2}` as 2nd argument
+    square_matrix(int n);                                      // constructor
+    square_matrix(int, const std::initializer_list<double> &); // constructor w two args
+    square_matrix(const square_matrix &);                      // copy constructor
+    square_matrix &operator=(const square_matrix &);
+    square_matrix &operator*=(const square_matrix &);
     ~square_matrix();
     friend std::ostream &operator<<(std::ostream &, const square_matrix &);
 
@@ -15,3 +19,15 @@ private:
     double *data = nullptr;
     int size;
 };
+
+square_matrix::square_matrix(int n, const std::initializer_list<double> &L)
+{
+    data = new double[n * n];
+    size_t index = 0;
+
+    for (auto i = L.begin(); i != L.end(); ++i)
+    {
+        data[index] = *i;
+        index++;
+    }
+} // better std::copy or std::ranges::copy from <algorithm>
--- exercises/class-matrix/readme.md
+++ exercises/class-matrix/readme.md
@@ -5,3 +5,22 @@ square_matrix fooBar (2); // creates instance fooBar
 ```
 ## Matrix practice:
 <!-- todo: add examples on how to use square_matrix class -->
+
+n x n matrix
+
+i-th row * j-th column
+
+these are the indies for row i ((i-1) * n), (i - 1) * n + 1
+
+{ j-1, j-1 + n, j-1 + 2n }
+Column j has indices:
+| 1 2 3 4 |
+| 5 6 7 8 |
+| 9 10 11 12 |
+| 13 14 15 16 |
+
+double sum = 0.0;
+for (int k = 0; k < n; ++k) 
+{
+    sum += result[(i - 1) * (n + k)] * result[(j-1) + (k*n)]
+}
