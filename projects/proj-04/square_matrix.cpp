bae99a12d66b90f492bd9347b7490273df6e740d
Luis Martinez <mrluismartinezzz@gmail.com>
2023-11-30 23:11:06 -0800

    [proj-4] 2. adds 2nd constructor

--- projects/proj-04/square_matrix.cpp
+++ projects/proj-04/square_matrix.cpp
@@ -4,4 +4,15 @@ square_matrix::square_matrix(int n)
 {
     size = n * n;
     data = new double[size];
+    for (int i = 0; i < size * size; ++i)
+    {
+        data[i] = 0.0;
+    }
+}
+
+square_matrix::square_matrix(int n, const std::initializer_list<double> &init_list)
+{
+    size = n * n;
+    data = new double[size];
+    std::copy(init_list.begin(), init_list.end(), data);
 }
--- projects/proj-04/square_matrix.h
+++ projects/proj-04/square_matrix.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <initializer_list>
+#include <algorithm>
 
 class square_matrix
 {
 public:
     square_matrix(int);
+    square_matrix(int, const std::initializer_list<double> &);
 
 private:
     int size;
