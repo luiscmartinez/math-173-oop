7a008174a9943e428358156551915b045ff6428c
Luis Martinez <mrluismartinezzz@gmail.com>
2023-11-30 23:39:45 -0800

    [proj-4] 3. defines copy constructor

--- projects/proj-04/square_matrix.cpp
+++ projects/proj-04/square_matrix.cpp
@@ -16,3 +16,12 @@ square_matrix::square_matrix(int n, const std::initializer_list<double> &init_li
     data = new double[size];
     std::copy(init_list.begin(), init_list.end(), data);
 }
+
+square_matrix::square_matrix(const square_matrix &matrix) : size(matrix.size)
+{
+    data = new double[size];
+    for (int i = 0; i < size; ++i)
+    {
+        data[i] = matrix.data[i];
+    }
+}
--- projects/proj-04/square_matrix.h
+++ projects/proj-04/square_matrix.h
@@ -8,6 +8,7 @@ class square_matrix
 public:
     square_matrix(int);
     square_matrix(int, const std::initializer_list<double> &);
+    square_matrix(const square_matrix &);
 
 private:
     int size;
